0721e6ba26e4b2c344f54ca5c52d70c2f8528c22
GabiMonteiros <monteiro8gabi@gmail.com>
2024-02-21 15:30:33 -0300

    nav dropdown01

---
+++ hthci-website/src/component/MenuItems.jsx
@@ -0,0 +1,26 @@
+export const MenuItems = [
+    {
+        title: 'After School Tutoring',
+        path: '/after-school',
+        cName: 'drpdown-link'
+    },
+    {
+        title: 'Scholarships',
+        path: '/scholarships',
+        cName: 'drpdown-link'
+    },
+    {
+        title: 'Public Health',
+        path: '/public-health',
+        cName: 'drpdown-link'
+    },{
+        title: 'Human Development',
+        path: '/human-development',
+        cName: 'drpdown-link'
+    },
+    {
+        title: 'Nature Based Solutions',
+        path: '/nature-based-solutions',
+        cName: 'drpdown-link'
+    }
+]
--- hthci-website/src/component/NavBar.jsx
+++ hthci-website/src/component/NavBar.jsx
@@ -2,16 +2,33 @@
 import React, { useState} from 'react'
 import { Link } from 'react-router-dom'
 import './NavBar.css'
-
+import Dropdown from './Dropdown'
 
 function Navbar() {
-    const [click, setClick] = useState(false)
+    const [click, setClick] = useState(false);
+    const [ dropdown, setDropdown] = useState(false);
     
     
-    const handleClick = () => setClick(!click)
-    const closeMobileMenu = () => setClick(false)
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
     
+
+    const onMouseEnter = () => {
+        if(window.innerWidth <960) {
+            setDropdown(false);
+        } else{
+            setDropdown(true);
+        }
+    };
     
+
+    const onMouseLeave = () => {
+        if(window.innerWidth <960) {
+            setDropdown(false);
+        } else{
+            setDropdown(false);
+        }
+    };
 
   return (
       <>
@@ -68,14 +85,19 @@ function Navbar() {
                               ABOUT US
                           </Link>
                       </li>
-                      <li className="nav-item">
+                      <li className="nav-item"
+                        onMouseEnter={onMouseEnter}
+                        onMouseLeave={onMouseLeave}
+                      >
                           <Link
                               to="/projects"
                               className="nav-links"
                               onClick={closeMobileMenu}
                           >
-                              PROJECTS
+                              PROJECTS <i className='fas fa-caret-down'/>
                           </Link>
+                          {dropdown && <Dropdown 
+                          />}
                       </li>
                       <li className="nav-item">
                           <Link
